6b356594e2d77c1bb6c93dc62311533b0f1ccb2c
Frank Femia <femiaf13@gmail.com>
2024-10-05 18:08:16 -0400

    tiny fixups

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/mrs-femias-word-game/",
   plugins: [vue({
     reactivityTransform: true
   })]
